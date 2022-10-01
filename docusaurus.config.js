b3da9a9de9c7641ee8612d206ff7c4979f25accf
chriteli1 <christelioglanidis@outlook.com>
2022-10-01 18:10:09 +0300

    Adjust copyright at the footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © 2022 morethanthemes.com - Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
