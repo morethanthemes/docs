3f980bd0121e935163475bae5b58d289d82c1428
Stavros Kounis <skounis@gmail.com>
2022-10-19 15:33:28 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/morethanthemes/docs/tree/master/',
+            'https://github.com/morethanthemes/docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
