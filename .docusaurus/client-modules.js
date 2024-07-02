564904a66d4c82f61d43241ab7d6f1cce693c78d
skounis <skounis@gmail.com>
2024-07-02 07:40:33 +0000

    Build: (4ee6435) Update changelog.md

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/workspaces/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/workspaces/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspaces/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspaces/docs/src/css/custom.css'),
-];
